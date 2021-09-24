1b493c9077b601298c3911c65bee7e1d320b414d
jhonvo <jhon26victor@gmail.com>
2021-09-24 16:48:01 -0500

    Updating content

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask( __name__ )
+
+@app.route ('/', methods=['GET'])
+def sayHello():
+    return 'Hello World'
+
+@app.route ('/dojo', methods=['GET'])
+def sayDojo():
+    return 'Dojo'
+
+@app.route ('/say/<msj>', methods=['GET'])
+def sayMsj(msj): 
+    return f"Hello {msj.capitalize()}!" 
+
+@app.route ('/repeat/<num>/<msj>', methods=['GET'])
+def multiplyMsj(num,msj): 
+    return msj * int(num)
+
+
+if __name__ == "__main__":
+    app.run( debug = True ) #This needs to do at te end of the app so it takes all the commands included above during the debug 
