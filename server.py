9cbf7092726668104f68fd1ad323b8e4f3b486a3
jhonvo <jhon26victor@gmail.com>
2021-09-24 17:26:23 -0500

    Adding the option to display custom message on error 404

--- server.py
+++ server.py
@@ -22,5 +22,20 @@ def multiplyMsj(num,msj):
         output += f"<h1>{msj}</h1>"
     return output
 
+@app.errorhandler(404) #app name
+def not_found(e): # inbuilt function which takes error as parameter
+    return "<h1>Sorry! No response. Try again.</h1>" #defining function - This could call as well a template
+
+# Errors
+# HTTP 300: MULTIPLE_CHOICES
+# HTTP 301: MOVED_PERMANENTLY
+# HTTP 302: FOUND
+# HTTP 303: SEE_OTHER
+# HTTP 304: NOT_MODIFIED
+# HTTP 305: USE_PROXY
+# HTTP 306: RESERVED
+# HTTP 307: TEMPORARY_REDIRECT
+# HTTP 302: NOT FOUND
+
 if __name__ == "__main__":
     app.run( debug = True ) #This needs to do at te end of the app so it takes all the commands included above during the debug 
